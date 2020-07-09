b0dcbf7288f102deb6463e633c32c8d4ee497a41
Dmitrii <dikkerd@ya.ru>
2020-07-09 17:21:24 +0300

    First Commit
    Basic Stuff

---
+++ src/main/java/screenRobotFF/WebBrowser.java
@@ -0,0 +1,59 @@
+package screenRobotFF;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.ProfilesIni;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class WebBrowser {
+
+    public static void autoScreenshot() {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd hh mm ss a");
+        Calendar now = Calendar.getInstance();
+        Robot robot = null;
+        try {
+            robot = new Robot();
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+        BufferedImage screenShot = robot.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+        try {
+            ImageIO.write(screenShot, "JPG", new File(formatter.format(now.getTime()) + ".jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void browserSetUp(){
+
+    }
+
+    public static void main(String[] args) throws IOException, AWTException {
+
+        System.setProperty("webdriver.gecko.driver", "C:/Users/dmitry.b/Documents/Geckodriver/geckodriver.exe");
+
+        ProfilesIni profile = new ProfilesIni();
+        FirefoxProfile testprofile = profile.getProfile("Robot");
+        FirefoxOptions opt = new FirefoxOptions();
+        opt.setProfile(testprofile);
+        FirefoxDriver driver = new FirefoxDriver(opt);
+
+        driver.get("http://ya.ru");
+        driver.findElement(By.tagName("body")).sendKeys(Keys.CONTROL + "t");
+        autoScreenshot();
+        driver.get("https://www.youtube.com/watch?v=N8Rr7rVf1RA");
+        autoScreenshot();
+        driver.close();
+
+    }
+}
